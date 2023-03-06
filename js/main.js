bf478f021f9a9122191f8c2d49caa483e5cbf943
alexcapricorngmailcom <107393171+alexcapricorngmailcom@users.noreply.github.com>
2023-03-06 16:13:19 +0200

    add ready task

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const tabQuestion = document.querySelectorAll('.faq-question');
+
+
+tabQuestion.forEach(el => {
+    el.addEventListener('click', function(event) {
+        const check = el.nextElementSibling.classList.contains('faq-answer__active');
+        
+        if (!check) {
+            el.nextElementSibling.classList.add('faq-answer__active');
+            el.lastElementChild.innerHTML = '-';
+        } else {
+            el.nextElementSibling.classList.remove('faq-answer__active');
+            el.lastElementChild.innerHTML = '+';
+        }
+    });
+})
